f3dada9a183ece55f90d358a5324410d844314e1
RomanCLV <55238742+RomanCLV@users.noreply.github.com>
2020-03-17 18:08:23 +0100

    restore master

--- TP_Dino/dino.php
+++ TP_Dino/dino.php
@@ -1,80 +1,73 @@
 <!DOCTYPE html>
+<?php
+    require 'Form.php';
+    $input = new Form();
+?>
 <html lang="fr">
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="../style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>¤ TP Reco ~ Dino ¤</title>
+    <title>¤ Dino ¤</title>
     <meta name="author" content="Roman CLAVIER">
 </head>
 <body>
 <?php include '../header.php' ?>
-<?php require 'Tyrex.php' ?>
-<?php require 'Triceratops.php' ?>
-<main>
-    <h2>Créer ton dino !</h2>
-    <form method="POST" action="#" id="formDino">
-        <?php
-        require 'Tools.php';
-        $tools = new Tools();
+<?php
+include 'Tyrex.php';
+include 'Triceratops.php';
+?>
 
-        echo "<div>";
-        $tools->CreateInput("number", "lifeDino", "Pts de vie : ", "PH");
-        echo "</div>";
-        echo "<div>";
-        $tools->CreateInput("number", "damageDino", "Dégats : ", "PA");
-        echo "</div>";
-
-        echo "<div>";
-        $tools->CreateInputRadio("sexDino", true, "Mâle");
-        $tools->CreateInputRadio("sexDino", false, "Femelle");
-        echo "</div>";
-
-        echo "<div>";
-        $tools->CreateSelect("typeDino", ["Tyrex", "Triceratops"]);
-        echo "</div>";
-
-        echo "<div>";
-        $tools->CreateTextArea("textAreaDino", "Résumé du combat : ");
-        echo "</div>";
-
-        echo "<div>";
-        $tools->CreateInputSubmit("validDino", "Créer mon dino");
-        echo "</div>";
-
-        if (!empty($_POST))
-        {
-            echo '<div id="myDinoDiv"><p>';
-            $myDino = null;
-            switch ($_POST["typeDino"])
-            {
-                case "Tyrex":
-                    $myDino = new Tyrex($_POST["lifeDino"], $_POST["damageDino"], $_POST["sexDino"]);
-                    break;
-                case "Triceratops":
-                    $myDino = new Triceratops($_POST["lifeDino"], $_POST["damageDino"], $_POST["sexDino"]);
-                    break;
-            }
-            echo '</p></div>';
-        }
-        ?>
-    </form>
-    <h2>Combat de dino !</h2>
-
+<form method="post" action="">
     <?php
-    $tyrex = new Tyrex(500, 50, true);
-    $triceratops = new Triceratops(600, 40, false);
+    $dinoSelect = array(
+        1 => 'Tyrex',
+        2 => 'Triceratops'
+    );
+    $genderSelect = array(
+        'Male' => 'Male',
+        'Femelle' => 'Femelle'
+    );
+
+    $input->createSelect('animalChoice', $dinoSelect, 'Choix du dinosaure');
+    echo '<br/>';
+    $input->createRadio($genderSelect, 'Choix du sex', 'gender');
+    echo '<br/>';
+    $input->createInput('number', "hp", null, "Combien de HP ?");
+    echo '<br/>';
+    $input->createInput("number", "damage", null, "Combien de dégats ?");
+    echo '<br/>';
+    $input->createTextarea("describe", 20, 40, "Histoire du dinosaure");
+    echo '<br/>';
+    $input->createInput("submit", "Envoyer", "Envoyer", null);
+    ?>
+</form>
+
+<?php
+
+if(isset($_POST['hp']) && isset($_POST['damage']) && isset($_POST['gender']) && isset($_POST['describe'])){
+    if($_POST['animalChoice'] == 1) {
+        $tyrex = new Tyrex($_POST['hp'], $_POST['damage'], $_POST['gender'], $_POST['describe']);
+        $triceratops = new Triceratops(rand(100, 500), rand(100,500), rand(1,2), "L'ennemi le plus coriace, un vrai dragon");
+    }
+    else{
+        $triceratops = new Triceratops($_POST['hp'], $_POST['damage'], $_POST['gender'], $_POST['describe']);
+        $tyrex = new Tyrex(rand(100, 500), rand(100,500), rand(1,2), "L'ennemi le plus coriace, un vrai dragon");
+    }
 
     $tyrex->attackSpecial($triceratops);
+    $tyrex->getLife();
     $triceratops->getLife();
-
     $tyrex->attackDouce($triceratops);
+    $tyrex->getLife();
     $triceratops->getLife();
-
     $triceratops->attackMortel($tyrex);
     $tyrex->getLife();
-    ?>
-</main>
+    $triceratops->getLife();
+}
+
+?>
+
 <?php include '../footer.php' ?>
 </body>
 </html>
