e5209ff500129d544a07075f4e94b6b0a387651b
Dreaxxx <ldqtheone@gmail.com>
2020-03-17 13:43:59 +0100

    Update US13.php

--- bdd/US13.php
+++ bdd/US13.php
@@ -2,8 +2,16 @@
 require 'Database.php';
 $database = new Database("php_week");
 ?>
-
-<h2 id="titleContact">Mes contactes :</h2>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title> Base de données </title>
+</head>
+<body>
+<?php include '../header.php' ?>
+<h2 id="titleContact">Mes contacts :</h2>
 
 <?php
 
@@ -15,4 +23,7 @@ foreach ($contacts as $contact){
     echo $contact['firstname'] . " " . $contact['lastname'] . " habite au : " . $contact['address'] . ".<br/>";
     echo "C'est un : " . $contact['gender'] . " et il est né le : " . $contact['birthdate'] . ".<br/>";
     echo "Son addresse email est : " . $contact['mail'] . ".<br/><br/>";
-}
+} ?>
+<?php include '../footer.php' ?>
+</body>
+</html>
