dd9cf8d5ec65e36a2086e7265a6fedeeaf192d65
Dreaxxx <ldqtheone@gmail.com>
2020-03-17 13:59:31 +0100

    Update php_week.sql

--- mysql/php_week.sql
+++ mysql/php_week.sql
@@ -48,16 +48,7 @@ CREATE TABLE IF NOT EXISTS `contacts` (
 --
 
 INSERT INTO `contacts` (`id`, `firstname`, `lastname`, `birthdate`, `gender`, `mail`, `address`) VALUES
-(1, 'Brian', 'lecarpentier', '1993-10-20', 'male', 'lecarp@hotmail.fr', '22 rue du centre'),
-(2, 'Abricoto', 'Coco', '1995-02-11', 'male', 'cocolabrico@gmail.com', '11 rue du bien heureux'),
-(3, 'Brian', 'lecarpentier', '8455-07-11', 'male', 'lecarp@hotmail.fe', '22 rue du centre'),
-(4, 'Dubou', 'Coco', '8114-07-11', 'male', 'toto@toto.com', '78 avenue du bonhomme'),
-(5, 'Brian', 'lecarpentier', '9414-08-17', 'male', 'lecarp@hotmail.free', '22 rue du centre'),
-(6, 'Lecarpentier', 'Brian', '4848-08-04', 'male', 'ldqtheone@gmail.come', '18 Rue Marguerite Yourcenar'),
-(7, 'Brian', 'lecarpentier', '1111-11-11', 'male', 'ldqtheone@gmail.comp', '22 rue du centre'),
-(8, 'Brian', 'lecarpentier', '1111-11-11', 'male', 'ldqtheone@gmail.comaz', '22 rue du centre'),
-(9, 'Lecarpentier', 'Brian', '1111-11-11', 'male', 'ldqtheone@gmail.comazs', '18 Rue Marguerite Yourcenar'),
-(10, 'Lecarpentier', 'Brian', '1111-11-11', 'male', 'ldqtheone@gmail.comwa', '18 Rue Marguerite Yourcenar');
+(1, 'Brian', 'lecarpentier', '1993-10-20', 'male', 'lecarp@hotmail.fr', '45 rue due saint andré');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
