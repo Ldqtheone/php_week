072bead605f78cb8a7cce37224b5bc38d49cb64e
vallois <corentin.vallois@edu.itescia.fr>
2020-03-18 16:37:50 +0100

    database fix

--- bdd/Database.php
+++ bdd/Database.php
@@ -17,7 +17,7 @@ class Database
     private function connexion(){
         try
         {
-            $bdd = new PDO('mysql:host=localhost;dbname='. $this->dbname, $this->login, $this->pass);
+            $bdd = new PDO('mysql:host=localhost:3306;dbname='. $this->dbname, $this->login, $this->pass);
             $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
             $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
             $this->connec = $bdd;
